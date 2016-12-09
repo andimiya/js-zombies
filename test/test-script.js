4f1d49d408a0862f6460d617518ccb6d7c79390e
Andrea <takamiya.andrea@gmail.com>
2016-12-08 20:44:56 -1000

    Test - Zombie should have a name parameter that is a string

--- test/test-script.js
+++ test/test-script.js
@@ -10,8 +10,17 @@ const Zombies = require('../zombies.js');
 describe('Item', () => {
   let item = Zombies.item;
 
-  it('should be a class', ()=> {
+  it('should be a class', () => {
     item.should.be.a.function;
   });
+
+  it('should have a name', () => {
+    item.name.should.equal(item.name);
+  });
+
+  it('name should be a string',() => {
+    item.name.should.be.a.string;
+  });
+
 
 });
