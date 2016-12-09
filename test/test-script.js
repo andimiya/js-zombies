2c179f013e244f1a1cd3a908f13b644bf6dfe32d
Andrea <takamiya.andrea@gmail.com>
2016-12-08 22:12:30 -1000

    Tests for Weapons added, added the beforeEach function to help me test my tests...I think

--- test/test-script.js
+++ test/test-script.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6 */
 
 const chai = require('chai');
+const expect = require('chai').expect;
 
 chai.expect();
 chai.should();
@@ -9,16 +10,39 @@ const Zombies = require('../zombies.js');
 
 describe('Item', () => {
 
-  let item = Zombies.item;
+  var item;
+  beforeEach(function(){
+    item = new Zombies.item('Bob');
+  });
 
-  // let item = new Zombies.item(12);  //Testing if my tests are right
-  // let item = new Zombies.item('Bob');  //Testing if my tests are right
-
-  it('should be a class', () => {
-    item.should.be.a.function;
+  it('should be a class', function() {
+    Zombies.item.should.be.a.function;
   });
 
   it('name should be a string',() => {
     item.name.should.be.a('string');
+  });
+});
+
+describe('Weapon', () => {
+  var item;
+  beforeEach(function(){
+    weapon = new Zombies.weapon('Sword', 12);
+  });
+
+  it('should be a class', function(){
+    Zombies.weapon.should.be.a.function;
+  });
+
+  it('should extend from the Item Class',() => {
+    weapon.should.be.instanceof(item);
+  });
+
+  it('name should be a string',() => {
+    weapon.name.should.be.a('string');
+  });
+
+  it('damage should be a number',() => {
+    weapon.damage.should.be.a('number');
   });
 });
