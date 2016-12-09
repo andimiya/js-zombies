3423a30e0757719c04db09944f9f67f47c3ebc53
Andrea <takamiya.andrea@gmail.com>
2016-12-08 21:12:53 -1000

    Tested my tests by invoking them, tests happened to be not written correctly so fixed the tests and re-committing

--- test/test-script.js
+++ test/test-script.js
@@ -8,19 +8,17 @@ chai.should();
 const Zombies = require('../zombies.js');
 
 describe('Item', () => {
+
   let item = Zombies.item;
+
+  // let item = new Zombies.item(12);  //Testing if my tests are right
+  // let item = new Zombies.item('Bob');  //Testing if my tests are right
 
   it('should be a class', () => {
     item.should.be.a.function;
   });
 
-  it('should have a name', () => {
-    item.name.should.equal(item.name);
-  });
-
   it('name should be a string',() => {
-    item.name.should.be.a.string;
+    item.name.should.be.a('string');
   });
-
-
 });
